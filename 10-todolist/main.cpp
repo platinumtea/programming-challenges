b5749fd2fab92e841cc42ee251de83efc134fde1
platinumtea <platinumtea7@gmail.com>
2020-04-21 20:55:08 -0400

    Create main.cpp

---
+++ 10-todolist/main.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+#include "Task.h"
+
+
+
+int main() {
+	std::vector<Task> taskList;
+	bool quit = false;
+
+	std::cout << "------------------------";
+	std::cout << "|                      |";
+	std::cout << "|  TO-DO LIST MANAGER  |";
+	std::cout << "|                      |";
+	std::cout << "------------------------";
+
+	while (!quit) {
+
+	}
+
+	return 0;
+}
+
+void addTask() {
+
+}
+
+void changeTask(int task) {
+
+}
+
+void deleteTask(int task) {
+
+}
+
+void saveList() {
+
+}
